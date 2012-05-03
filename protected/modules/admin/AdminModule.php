2e938cb0c2982eb7afa25b77cda64165a486e011
Dipesh Acharya <xtranophilist@gmail.com>
2012-05-04 02:43:33 +0545

    added settings in admin

--- protected/modules/admin/AdminModule.php
+++ protected/modules/admin/AdminModule.php
@@ -8,7 +8,6 @@ class AdminModule extends CWebModule {
         // this method is called when the module is being created
         // you may place code here to customize the module or the application
         // import the module-level models and components
-        //$this->setModules(array('user'));
         $this->setImport(array(
             'admin.models.*',
             'admin.components.*',
@@ -19,6 +18,35 @@ class AdminModule extends CWebModule {
 
     public function beforeControllerAction($controller, $action) {
         if (parent::beforeControllerAction($controller, $action)) {
+
+
+            if (Yii::app()->user->isGuest) {
+                $model = new UserLogin;
+                //$this->redirect('/');
+                //die();
+                $this->render('/user/login', array('model' => $model));
+            } elseif (UserModule::isAdmin()) {
+
+                $dataProvider = new CActiveDataProvider('User', array(
+                            'criteria' => array(
+                                'condition' => 'status>' . User::STATUS_BANED,
+                            ),
+                            'pagination' => array(
+                                'pageSize' => Yii::app()->controller->module->user_page_size,
+                            ),
+                        ));
+                $this->render('/user/index', array(
+                    'dataProvider' => $dataProvider,
+                ));
+            } else {
+                $this->_model = Yii::app()->controller->module->user();
+                $this->render('/profile/profile', array(
+                    'model' => $this->_model,
+                    'profile' => $this->_model->profile,
+                ));
+            }
+
+
             // this method is called before any module controller action is performed
             $controller->layout = 'main';
             return true;
@@ -26,8 +54,8 @@ class AdminModule extends CWebModule {
         else
             return false;
     }
-    
-    public static function t($a){
+
+    public static function t($a) {
         return $a;
     }
 
---
+++ protected/modules/admin/controllers/SettingsController.php
@@ -0,0 +1,12 @@
+<?php
+
+class SettingsController extends Controller {
+
+    public function actionIndex() {
+        
+        
+        
+        $this->render('index');
+    }
+
+}
