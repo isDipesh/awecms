53bba368286b43dc99f777b952479b42afe41ebe
Dipesh Acharya <xtranophilist@gmail.com>
2012-06-17 19:55:07 +0545

    deleted user and category module and updated AweUrlManager so that it doesn't mess with Gii

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
 $yii = dirname(__FILE__) . '/../../yii/framework/yii.php';
+
 date_default_timezone_set('GMT');
+
 // remove the following line when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG', true);
-// specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
 
 $mainConfig = dirname(__FILE__) . '/protected/config/main.php';
 
--- protected/components/AweUrlManager.php
+++ protected/components/AweUrlManager.php
@@ -3,15 +3,23 @@
 class AweUrlManager extends CUrlManager {
 
     public function createUrl($route, $params = array(), $ampersand = '&') {
+
         //for admin and admin/*
         $parts = explode('/', $route);
+
+        //do not mess with gii
+        if ($parts[0] == 'gii')
+            return parent::createUrl($route, $params, $ampersand);
+
         if (Yii::app()->hasModule($parts[0]) && isset($parts[1]) && $parts[1] == Yii::app()->getModule($parts[0])->defaultController) {
             unset($parts[1]);
             return implode('/', $parts);
         }
+
         if (substr(Yii::app()->getRequest()->pathInfo, 0, 6) == 'admin/' || Yii::app()->getRequest()->pathInfo == 'admin') {
             return '/admin' . parent::createUrl($route, $params, $ampersand);
         }
+
         return parent::createUrl($route, $params, $ampersand);
     }
 
