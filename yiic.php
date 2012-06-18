9b549794e21c85f97ca60d7decbe5887bd002299
RohitRox <link2j.roy@gmail.com>
2012-06-19 01:42:29 +0545

    migration script fixes

---
+++ yiic.php
@@ -0,0 +1,8 @@
+<?php
+
+// change the following paths if necessary
+$yiic=dirname(__FILE__).'/../../yii/framework/yiic.php';
+$config=dirname(__FILE__).'/protected/config/console.php';
+
+require_once($yiic);
+
