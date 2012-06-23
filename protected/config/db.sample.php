29756cd552508a17a6234e86e27e015ee2f58a55
Dipesh Acharya <xtranophilist@gmail.com>
2012-06-23 15:35:15 +0545

    fixed db.sample.php

--- protected/config/db.sample.php
+++ protected/config/db.sample.php
@@ -1,11 +1,16 @@
 <?php
+
 return array(
+    'components' => array(
+        'db' => array(
             'connectionString' => 'mysql:host=localhost;dbname=awecms',
             'emulatePrepare' => true,
             'username' => 'root',
-            'password' => '',
+            'password' => 'password',
             'charset' => 'utf8',
             'tablePrefix' => '',
             'enableProfiling' => true,
             'enableParamLogging' => true,
-        );
+        )
+    )
+);
