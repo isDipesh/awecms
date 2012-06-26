1181cad174eb9db29f04365c0c379a030523314a
Dipesh Acharya <xtranophilist@gmail.com>
2012-06-27 00:36:09 +0545

    [category] fixed category create/update form not to allow pages selection

--- protected/modules/category/controllers/CategoryController.php
+++ protected/modules/category/controllers/CategoryController.php
@@ -1,7 +1,8 @@
 <?php
+
 class CategoryController extends Controller {
 
-public function filters() {
+    public function filters() {
         return array(
             'accessControl',
         );
@@ -21,103 +22,96 @@ public function filters() {
     public function actionIndex() {
         $dataProvider = new CActiveDataProvider('Category');
         $this->render('index', array(
-                'dataProvider' => $dataProvider,
+            'dataProvider' => $dataProvider,
         ));
     }
-        
+
     public function actionView($id) {
         $this->render('view', array(
-                'model' => $this->loadModel($id),
+            'model' => $this->loadModel($id),
         ));
     }
-        
+
     public function actionCreate() {
         $model = new Category;
-                if (isset($_POST['Category'])) {
+        if (isset($_POST['Category'])) {
             $model->setAttributes($_POST['Category']);
 
-			 if (isset($_POST['Category']['pages'])) $model->pages = $_POST['Category']['pages'];
-                
-                try {
-                    if($model->save()) {
+            try {
+                if ($model->save()) {
                     if (isset($_GET['returnUrl'])) {
-                            $this->redirect($_GET['returnUrl']);
+                        $this->redirect($_GET['returnUrl']);
                     } else {
-                            $this->redirect(array('view','id'=>$model->id));
+                        $this->redirect(array('view', 'id' => $model->id));
                     }
                 }
-                } catch (Exception $e) {
-                        $model->addError('', $e->getMessage());
-                }
-        } elseif(isset($_GET['Category'])) {
-                        $model->attributes = $_GET['Category'];
+            } catch (Exception $e) {
+                $model->addError('', $e->getMessage());
+            }
+        } elseif (isset($_GET['Category'])) {
+            $model->attributes = $_GET['Category'];
         }
 
-        $this->render('create',array( 'model'=>$model));
+        $this->render('create', array('model' => $model));
     }
 
     public function actionUpdate($id) {
         $model = $this->loadModel($id);
-        
-        if(isset($_POST['Category'])) {
+
+        if (isset($_POST['Category'])) {
             $model->setAttributes($_POST['Category']);
-			$model->pages = $_POST['Category']['pages'];
-                try {
-                    if($model->save()) {
-                        if (isset($_GET['returnUrl'])) {
-                                $this->redirect($_GET['returnUrl']);
-                        } else {
-                                $this->redirect(array('view','id'=>$model->id));
-                        }
+            try {
+                if ($model->save()) {
+                    if (isset($_GET['returnUrl'])) {
+                        $this->redirect($_GET['returnUrl']);
+                    } else {
+                        $this->redirect(array('view', 'id' => $model->id));
                     }
-                } catch (Exception $e) {
-                        $model->addError('', $e->getMessage());
                 }
-
+            } catch (Exception $e) {
+                $model->addError('', $e->getMessage());
             }
+        }
 
-        $this->render('update',array(
-                'model'=>$model,
-                ));
+        $this->render('update', array(
+            'model' => $model,
+        ));
     }
-                
-               
 
     public function actionDelete($id) {
-        if(Yii::app()->request->isPostRequest) {    
+        if (Yii::app()->request->isPostRequest) {
             try {
                 $this->loadModel($id)->delete();
             } catch (Exception $e) {
-                    throw new CHttpException(500,$e->getMessage());
+                throw new CHttpException(500, $e->getMessage());
             }
 
             if (!Yii::app()->getRequest()->getIsAjaxRequest()) {
-                            $this->redirect(array('admin'));
+                $this->redirect(array('admin'));
             }
         }
         else
             throw new CHttpException(400,
-                Yii::t('app', 'Invalid request.'));
+                    Yii::t('app', 'Invalid request.'));
     }
-                
+
     public function actionAdmin() {
         $model = new Category('search');
         $model->unsetAttributes();
 
         if (isset($_GET['Category']))
-                $model->setAttributes($_GET['Category']);
+            $model->setAttributes($_GET['Category']);
 
         $this->render('admin', array(
-                'model' => $model,
+            'model' => $model,
         ));
     }
-    
-    
+
     public function loadModel($id) {
-            $model=Category::model()->findByPk($id);
-            if($model===null)
-                    throw new CHttpException(404,Yii::t('app', 'The requested page does not exist.'));
-            return $model;
+        $model = Category::model()->findByPk($id);
+        if ($model === null)
+            throw new CHttpException(404, Yii::t('app', 'The requested page does not exist.'));
+        return $model;
     }
 
 }
