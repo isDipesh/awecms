baea7e6cec06b0a8d111c43a90c8d26692185478
RohitRox <link2j.roy@gmail.com>
2012-06-27 00:06:08 +0545

    fixed js in select box changes in roles, access management update page

--- protected/modules/role/assets/accessForm.js
+++ protected/modules/role/assets/accessForm.js
@@ -1,16 +1,43 @@
 $(document).ready(function(){
 
+    var param_id = getUrlVars()["id"];
+
     var module = $('#Access_module').val();
 
     $('#Access_module').change(function(){
+
         var loc = document.location.host+document.location.pathname;
         loc += '?module='+$(this).val();
-        self.location.href = 'http://'+loc
+
+        if (param_id == undefined ){
+            window.location.href = 'http://'+loc
+        }
+        else
+        {
+            window.location.href = 'http://'+loc+'&id='+param_id;
+        }
     });
 
     $('#Access_controller').change(function(){
+
         var loc = document.location.host+document.location.pathname;
         loc += '?module='+module+'&controller='+$(this).val();
-        window.location.href = 'http://'+loc
+
+        if (param_id == undefined ){
+            window.location.href = 'http://'+loc
+        }
+        else
+        {
+            window.location.href = 'http://'+loc+'&id='+param_id;
+        }
+
     });
 });
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
