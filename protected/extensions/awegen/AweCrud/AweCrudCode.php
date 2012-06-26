38a935449c31531942b274945d86a5e2d34bc759
Dipesh Acharya <xtranophilist@gmail.com>
2012-06-27 00:37:51 +0545

    [awegen] fixed harmless '\CHtml' typo in AweCrudCode

--- protected/extensions/awegen/AweCrud/AweCrudCode.php
+++ protected/extensions/awegen/AweCrud/AweCrudCode.php
@@ -146,7 +146,7 @@ class AweCrudCode extends CrudCode {
         $foreign_identificationColumn = self::getIdentificationColumnFromTableSchema(CActiveRecord::model($relation[1])->getTableSchema());
         $friendlyName = ucfirst($relatedModelClass);
         $str = "<label for=\"$relatedModelClass\"><?php echo Yii::t('app', '$friendlyName'); ?></label>\n";
-        $str .= "<?php echo \CHtml::checkBoxList('{$modelClass}[{$relatedModelClass}]', array_map('Awecms::getPrimaryKey',\$model->{$relatedModelClass}),
+        $str .= "<?php echo CHtml::checkBoxList('{$modelClass}[{$relatedModelClass}]', array_map('Awecms::getPrimaryKey',\$model->{$relatedModelClass}),
             CHtml::listData({$relation[1]}::model()->findAll(),'{$foreign_pk}', '{$foreign_identificationColumn}'),
             array('attributeitem' => '{$foreign_pk}', 'checkAll' => 'Select All')); ?>";
         return $str;
