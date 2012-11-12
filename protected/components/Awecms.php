85998ebec2b3248b977c96ad80214e41d2407fed
Dipesh Acharya <xtranophilist@gmail.com>
2012-11-13 00:30:34 +0545

    made page generation time more accurate

--- protected/components/Awecms.php
+++ protected/components/Awecms.php
@@ -5,15 +5,15 @@ class Awecms {
 
     const version = '0.4b';
 
-    public static $start_time;
     public static $tmp;
 
     public static function powered($stats = false) {
         echo Yii::t('app', 'Powered by') . ' ';
         echo '<a href="http://github.com/awecms" target="_blank">AweCMS</a>.<br />';
         //show page stats when in development mode or when explicitly asked
-        if ($stats || YII_DEBUG)
-            echo 'Page generated in ' . round((microtime(TRUE) - Awecms::$start_time), 4) . ' seconds using ' . round(memory_get_peak_usage(true) / 1048576, 2) . ' MB of memory!';
+        if ($stats || YII_DEBUG) {
+            echo 'Page generated in ' . round((microtime(TRUE) - YII_BEGIN_TIME), 4) . ' seconds using ' . round(memory_get_peak_usage(true) / 1048576, 2) . ' MB of memory!';
+        }
     }
 
     public static function getPrimaryKey($ar) {
