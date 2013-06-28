97f0e584a0532e6e18f264b710e2aa2be1509722
Dipesh Acharya <xtranophilist@gmail.com>
2013-06-28 12:13:53 +0545

    fixed HTML validation errors

--- themes/found/views/layouts/main.php
+++ themes/found/views/layouts/main.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="<?php echo Yii::app()->language ?>">
+<html prefix="og: http://ogp.me/ns#">
 
 <head>
     <meta charset="utf-8" />
@@ -8,47 +8,51 @@
 
     <link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/normalize.css">
     <link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/foundation.css">
+    <link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/base.css">
 
     <script src="<?php echo Yii::app()->theme->baseUrl; ?>/js/vendor/custom.modernizr.js"></script>
 </head>
 
 <body itemscope itemtype="http://schema.org/<?php echo $this->webpageType; ?>">
-    <div class="wrapper">
-        <header itemtype="http://schema.org/WPHeader">
-            <a href="<?php echo Yii::app()->baseUrl; ?>/">
-                <h1 class="head"><?php echo Settings::get('site', 'name'); ?></h1>
-            </a>
-            <?php $this->widget('SearchBlock'); ?>
-            <?php $this->widget('LoginWidget'); ?>
-            <?php // $this->widget('LanguagePicker'); ?>
-            <nav id="nav" itemscope itemtype="http://schema.org/SiteNavigationElement" >
-                <?php $this->widget('MenuRenderer'); ?>
-            </nav>
-        </header>
 
-        <div class="mid-bar row">
-            <?php //TODO: show up some message and breadcrumb like Home /Index when user is not signed in and is in home page  ?>
-            <?php
-            if (((Settings::get('site', 'enable_breadcrumbs') == '') || (Settings::get('site', 'enable_breadcrumbs') == 1)) && isset($this->breadcrumbs)) {
-                $this->widget('Breadcrumbs', array(
-                    'links' => $this->breadcrumbs,
-                    ));
-            }
-            ?>
-        </div>
+    <header itemscope itemtype="http://schema.org/WPHeader" class="row">
+        <a href="<?php echo Yii::app()->baseUrl; ?>/">
+            <h1 class="head"><?php echo Settings::get('site', 'name'); ?></h1>
+        </a>
+        <?php $this->widget('SearchBlock'); ?>
+        <?php $this->widget('LoginWidget'); ?>
+        <?php // $this->widget('LanguagePicker'); ?>
+        <nav id="nav" itemscope itemtype="http://schema.org/SiteNavigationElement" >
+            <?php $this->widget('MenuRenderer'); ?>
+        </nav>
+    </header>
 
-        <div class="row">
-            <div class="fifth" itemtype="http://schema.org/WPSideBar">
-               <?php $this->widget('MenuRenderer', array('id'=>3)); ?>
-               <?php $this->widget('Events'); ?>
-           </div>
-
-           <div class="fourfifth" itemprop="mainContentOfPage" itemtype="http://schema.org/WebPageElement">
-            <?php echo $content; ?>
-        </div>
+    <div class="row crumb">
+        <?php //TODO: show up some message and breadcrumb like Home /Index when user is not signed in and is in home page  ?>
+        <?php
+        if (((Settings::get('site', 'enable_breadcrumbs') == '') || (Settings::get('site', 'enable_breadcrumbs') == 1)) && isset($this->breadcrumbs)) {
+            $this->widget('Breadcrumbs', array(
+                'links' => $this->breadcrumbs,
+                ));
+        }
+        ?>
     </div>
-    <?php include_once '_footer.php'; ?>
+
+    <div class="row main">
+
+        <div itemscope itemtype="http://schema.org/WPSideBar">
+           <?php $this->widget('MenuRenderer', array('id'=>3)); ?>
+           <?php $this->widget('Events'); ?>
+       </div>
+
+       <div itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/WebPageElement">
+        <?php echo $content; ?>
+    </div>
+
 </div>
+
+<?php include_once '_footer.php'; ?>
 <?php $this->widget('GAnalytics'); ?>
+
 </body>
 </html>
